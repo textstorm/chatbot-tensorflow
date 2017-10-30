b9b44c0ddf307d207f1cf38a142a749695f2c486
textstorm <jiangzhewise@gmail.com>
2017-10-30 17:54:20 +0800

    utils update

--- utils.py
+++ utils.py
@@ -4,6 +4,7 @@ import tensorflow as tf
 import numpy as np
 import sys
 import time
+import re
 
 #data utils
 def load_data(file_dir):
@@ -216,3 +217,17 @@ def get_config_proto(log_device_placement=False, allow_soft_placement=True):
       allow_soft_placement=allow_soft_placement)
   config_proto.gpu_options.allow_growth = True
   return config_proto
+
+# Turn a Unicode string to plain ASCII, thanks to
+# http://stackoverflow.com/a/518232/2809427
+def unicodeToAscii(s):
+  return ''.join(
+    c for c in unicodedata.normalize('NFD', s)
+      if unicodedata.category(c) != 'Mn')
+
+# Lowercase, trim, and remove non-letter characters
+def normalizeString(s):
+  s = unicodeToAscii(s.lower().strip())
+  s = re.sub(r"([.!?])", r" \1", s)
+  s = re.sub(r"[^a-zA-Z.!?]+", r" ", s)
+  return s
